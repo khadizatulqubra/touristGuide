1488f7b86649e769fe435a1775da0ea56bcb9f50
khadizatulqubra <khadizatul.qubra@chasacademy.se>
2024-07-04 18:52:06 +0200

    touristpath first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, HeaderComponent,HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
