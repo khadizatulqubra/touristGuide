96befceebc7fa56e8b87eb717e3315e02e41abf3
khadizatulqubra <khadizatul.qubra@chasacademy.se>
2024-07-22 13:20:19 +0200

    new update

--- src/app/google.service.ts
+++ src/app/google.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GoogleService {
+export class GooglePlacesService {
+  private apiUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+  private apiKey = 'YOUR_GOOGLE_PLACES_API_KEY'; // Replace with your Google Places API key
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) {}
+
+  getPopularPlaces(location: string, radius: number = 5000): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?location=${location}&radius=${radius}&key=${this.apiKey}`);
+  }}
--- src/app/popular-destinations/popular-destinations.component.ts
+++ src/app/popular-destinations/popular-destinations.component.ts
@@ -1,21 +1,49 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule, NgStyle } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { UnsplashService } from '../unsplash.service';
+import { GooglePlacesService } from '../google.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-popular-destinations',
   standalone: true,
-  imports: [CommonModule,NgStyle], // Ensure CommonModule is imported
+  imports: [CommonModule,NgStyle], 
   templateUrl: './popular-destinations.component.html',
   styleUrls: ['./popular-destinations.component.css']
 })
 export class PopularDestinationsComponent implements OnInit {
-  @Input() color: string | undefined;
+  destinations: any[] = [];
 
-  constructor() {}
+  constructor(
+    private unsplashService: UnsplashService,
+    private googlePlacesService: GooglePlacesService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    // Replace '37.7749,-122.4194' with the location you want to search for
+    const location = '37.7749,-122.4194'; // Example location (San Francisco)
+    
+    forkJoin({
+      unsplash: this.unsplashService.getPopularPlaces(),
+      googlePlaces: this.googlePlacesService.getPopularPlaces(location)
+    }).subscribe(
+      ({ unsplash, googlePlaces }) => {
+        // Combine data from both APIs
+        this.destinations = googlePlaces.results.map((place: { name: any; vicinity: any; }, index: string | number) => ({
+          title: place.name,
+          description: place.vicinity,
+          image: unsplash.results[index]?.urls.small || 'default-image-url', // Fallback image
+        }));
+      },
+      error => {
+        console.error('Error fetching destinations:', error);
+      }
+    );
+  }
+}
 
+/* 
   destinations = [
     {
       image: '/assets/Venice.jpg',
@@ -62,5 +90,4 @@ export class PopularDestinationsComponent implements OnInit {
       title: 'Cyprus',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     }
-  ];
-}
+  ]; */
